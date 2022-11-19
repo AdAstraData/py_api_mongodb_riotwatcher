007e441c66e33e303afb57227bd7364fc7d47ec6
AdAstraData <hugoafsantos@outlook.com>
2022-11-19 13:21:30 +0000

    UPDATE initial script

--- trigger_py.py
+++ trigger_py.py
@@ -1,2 +1,11 @@
-print("Hello")
-print("Hello")
+"""
+Selenium-based scraping Google Flights
+Lisbon, Portugal - Nov 19, 2022
+"""
+
+from selenium_scraps import scrap_google_flights
+
+import pandas as pd
+import numpy as np
+
+scrap_google_flights()
