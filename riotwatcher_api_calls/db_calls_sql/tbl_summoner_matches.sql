c2d5c056a053b09a7dd898ac81ce5d34fa26f8e5
AdAstraData <hugoafsantos@outlook.com>
2022-11-27 03:46:14 +0000

    OPTIMIZE API call to fetch matches and only annotate AND push those that were not yet seen (not part of DB)

--- riotwatcher_api_calls/db_calls_sql/tbl_summoner_matches.sql
+++ riotwatcher_api_calls/db_calls_sql/tbl_summoner_matches.sql
@@ -5,6 +5,12 @@ SELECT * FROM [DataScience].[dbo].tbl_summoner_matches
 ORDER BY match_start_date desc, match_start_time desc
 
 /*
+SELECT *
+FROM [DataScience].[dbo].tbl_summoner_matches
+WHERE match_id = 'EUW1_6166598877'
+*/
+
+/*
 SELECT * FROM [DataScience].[dbo].tbl_summoner_matches
 WHERE summoner_name = 'AdAstraData'
 ORDER BY integration_datetime desc
@@ -16,8 +22,40 @@ ORDER BY integration_datetime desc
 SELECT * FROM [DataScience].[dbo].tbl_summoner_matches
 WHERE summoner_name NOT IN ('Storm and Sin', 'AdAstraData')
 ORDER BY integration_datetime desc
+*/
 
-SELECT *
+/*
+SELECT
+	match_id
+	,min_since_match_end = DATEDIFF(MINUTE, CAST(CONCAT(match_end_date,' ',match_end_time) AS DATETIME), integration_datetime)
+
 FROM [DataScience].[dbo].tbl_summoner_matches
-WHERE match_id = 'EUW1_6122482710'
+ORDER BY min_since_match_end asc
+*/
+
+/*
+;WITH tmp_tbl_a AS (
+	SELECT
+		today_date = GETDATE()
+		,summoner_name
+		,num = COUNT(*)
+	FROM [DataScience].[dbo].[tbl_summoner_matches]
+	GROUP BY summoner_name
+	),
+
+tmp_tbl_b AS (
+	SELECT
+		summoner_name
+		,num
+		,total_num = SUM(num) OVER (PARTITION BY today_date)
+
+	FROM tmp_tbl_a
+	)
+
+SELECT
+	*
+	,perc = CAST(ROUND(100.0 * num/total_num, 2) AS FLOAT(2))
+
+FROM tmp_tbl_b
+ORDER BY perc desc
 */
