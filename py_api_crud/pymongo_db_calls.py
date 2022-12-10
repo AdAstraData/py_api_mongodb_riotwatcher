d84ea408e95c28a8cda0b9b38e8e1ad564e6b6e7
AdAstraData <hugoafsantos@outlook.com>
2022-12-10 21:46:55 +0000

    UPDATE py_api_mongodb_riotwatcher

---
+++ py_api_crud/pymongo_db_calls.py
@@ -0,0 +1,23 @@
+from riotwatcher import LolWatcher, ApiError
+
+from dotenv import dotenv_values
+config = dotenv_values("../.env.local")
+
+from pymongo import MongoClient
+
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://"+config['MONGODB_USER']+":"+config['MONGODB_PWD']+"@cluster.mongodb.net/"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['user_shopping_list']
+  
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":   
+  
+   # Get the database
+   dbname = get_database()
